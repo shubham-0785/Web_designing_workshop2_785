2a78a73350233b1ae972663ea159a23e591028a1
SHUBHAM <shubhamyt88877@gmail.com>
2026-06-22 22:33:07 +0530

    Exp 11

---
+++ EXPERIMENTS/exp11/studentapi/src/main/java/com/example/studentapi/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.studentapi;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Student Management REST API";
+    }
+
+    @GetMapping("/student")
+    public Student getStudent() {
+        return new Student(
+                101,
+                "SATYAM",
+                "Computer Science");
+    }
+
+    @PostMapping("/student")
+    public Student addStudent(@RequestBody Student student) {
+        return student;
+    }
+}
